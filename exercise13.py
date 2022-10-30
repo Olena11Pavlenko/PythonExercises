8c0734d1bae78fdf2eeb658a0674a10bae7c18ee
Olena11Pavlenko <114681305+Olena11Pavlenko@users.noreply.github.com>
2022-10-30 18:38:17 +0200

    ex13

--- exercise13.py
+++ exercise13.py
@@ -1,20 +1,16 @@
-#https://edabit.com/challenge/6JNHBeGxY8dhTaPhs
+# 3n + 1 Problem (Collatz Conjecture)
+# https://edabit.com/challenge/6JNHBeGxY8dhTaPhs
+def count_steps (num):
+    steps = 0
+    while num!=1:
+        if num %2 == 0:
+            num = num/2
+        else: 
+            num = 3*num + 1
+        steps +=1
+    return steps
 def collatz(a, b):
-    steps_a = 0
-    while a!=1:
-        if a %2 == 0:
-            a = a/2
-        else: 
-            a = 3*a + 1
-        steps_a +=1
-    steps_b = 0
-    while b!=1:
-        if b %2 == 0:
-            b = b/2
-        else: 
-            b = 3*b + 1
-        steps_b +=1
-    if steps_a<steps_b:
+    if count_steps(a)<count_steps(b):
         return "a"
     else:
         return "b"
