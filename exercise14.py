37ed0c05748386026ebf6b36ed51849ff1c72851
Olena11Pavlenko <114681305+Olena11Pavlenko@users.noreply.github.com>
2022-11-03 11:10:02 +0200

    ex14

--- exercise14.py
+++ exercise14.py
@@ -9,5 +9,5 @@ def majority_vote(lst):
 print (majority_vote(["A", "A", "B"]))
 print (majority_vote(["A", "A", "A", "B", "C", "A"]))
 print (majority_vote(["A", "B", "B", "A", "C", "C"]))
-print (majority_vote(["A", "B", "B", "B"]))
+print (majority_vote(["A", "B", "B", "B", "C", "B"]))
 print (majority_vote([]))
