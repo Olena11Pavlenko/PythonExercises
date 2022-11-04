a44a666eb5344e17c954edbb03011bedcbc83d66
Helen <114681305+Olena11Pavlenko@users.noreply.github.com>
2022-11-04 18:35:22 +0200

    Majority Vote (#2)
    * ex14
    
    * ex14

--- exercise14.py
+++ exercise14.py
@@ -2,12 +2,12 @@
 # https://edabit.com/challenge/pQavNkBbdmvSMmx5x
 
 def majority_vote(lst):
-    for i in lst:
-        if lst.count(i) > len(lst)/2:
-            return i
+    for vote in set(lst):
+        if lst.count(vote) > len(lst)/2:
+            return vote
 
 print (majority_vote(["A", "A", "B"]))
 print (majority_vote(["A", "A", "A", "B", "C", "A"]))
 print (majority_vote(["A", "B", "B", "A", "C", "C"]))
-print (majority_vote(["A", "B", "B", "B"]))
+print (majority_vote(["A", "B", "B", "B", "C", "B"]))
 print (majority_vote([]))
