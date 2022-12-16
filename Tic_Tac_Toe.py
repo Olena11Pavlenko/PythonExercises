0f89bb7a698f002b49e3b1513e2797087b987160
Olena11Pavlenko <114681305+Olena11Pavlenko@users.noreply.github.com>
2022-12-16 20:30:40 +0800

    3 task

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -6,27 +6,37 @@
 
 import numpy as np
 def tic_tac_toe(inputs):
-    num = 1000
+    num1 = 1000
+    num2 = 100
     inputs = np.array(inputs, dtype="object")
-    inputs[inputs == "X"] = num
-    inputs[inputs == "#"] = 0
-    inputs[inputs == "O"] = 1
+    inputs = np.where(inputs == "X", num1, inputs)
+    inputs = np.where(inputs == "@", num2, inputs)
+    inputs = np.where(inputs == "#", 0, inputs)
+    inputs = np.where(inputs == "O", 1, inputs)
+    #inputs[inputs == "X"] = num1
+    #inputs[inputs == "@"] = num2
+    #inputs[inputs == "#"] = 0
+    #inputs[inputs == "O"] = 1
     inputs=inputs.astype(int)
     sum_by_columns = np.sum(inputs, axis = 0)
+    print (sum_by_columns)
     sum_by_rows = np.sum(inputs, axis = 1)
     diagonal1 = np.diagonal(inputs)
     diagonal2 = np.diag(np.fliplr(inputs))
     num_rows = len(inputs)
     print (inputs)
     if np.min(sum_by_columns) == num_rows or np.min(sum_by_rows) == num_rows or np.sum(diagonal1) == num_rows or np.sum(diagonal2) == num_rows:
-        return "Player 2 wins"
-    elif np.max(sum_by_columns) == num_rows*num or np.max(sum_by_rows) == num_rows*num or np.sum(diagonal1) == num_rows*num or np.sum(diagonal2) == num_rows*num:
-        return "Player 1 wins"
+        return "Player O wins"
+    elif np.max(sum_by_columns) == num_rows*num1 or np.max(sum_by_rows) == num_rows*num1 or np.sum(diagonal1) == num_rows*num1 or np.sum(diagonal2) == num_rows*num1:
+        return "Player X wins"
+    elif num2*num_rows in sum_by_columns or num2*num_rows in sum_by_rows or np.mean(diagonal1) == num2 or np.mean(diagonal2) == num2:
+        return "Player @ wins"
     else:
         return "It's a Tie"
 
 print (tic_tac_toe([
-  ["X", "X", "O"],
-  ["O", "X", "O"],
-  ["X", "O", "#"]
+  ["X", "X", "O", "@"],
+  ["O", "X", "O", "@"],
+  ["X", "@", "O", "O"],
+  ["@", "X", "O", "@"]
 ]))
